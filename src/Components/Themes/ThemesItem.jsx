1e5096a58a73145c6a18ca595c002a582559eef5
Himanshu <Himanshusingh0909@gmail.com>
2024-05-19 09:29:25 +0530

    ...

--- src/Components/Themes/ThemesItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ThemesItem({ color, img, changeColor }) {
-    return (
-        <img src={img} className="theme-img" onClick={() => { changeColor(color) }} />
-    )
-}
-
-export default ThemesItem;
